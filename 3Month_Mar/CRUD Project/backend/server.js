4a429050169b63160244c106f59ef35bf4ba10c6
pawarmahendra46 <pawarmahendra46@gmail.com>
2026-03-07 20:42:24 +0530

    code change updated 07-03-2026

--- 3Month_Mar/CRUD Project/backend/server.js
+++ 3Month_Mar/CRUD Project/backend/server.js
@@ -2,3 +2,119 @@
 //node js
 //express js
 //Db-MongoDb
+
+//API List
+//1.API Create Item ==>get data from front end and save in Db
+
+//2.Api Update Item ==> get/update data from front end and update in db
+
+//3.API Delete item ==> get details from front end and Delete in Db
+
+//4.API get all item ==> get from db and show at front end.
+
+console.log('Welcome Mahendra.')
+
+const express = require('express') //express js is a framework of node js.
+
+const mongoose = require('mongoose') //library connect mongoDb
+const cors=require('cors') //library
+
+const app = express()// app- var- store express function
+app.use(express.json()) //convert all data into json format.
+app.use(cors())
+//Dbconnection
+mongoose.connect("mongodb://127.0.0.1:27017/crud-Database-1").then(() => console.log("Mongo DB Connected..")).catch((err) => console.log(err));
+
+app.use(express.json());
+//Schema- means model/table structure
+const itemSchema = new mongoose.Schema({
+    name: String,
+    description: String,
+    sellingPrice: Number,
+    purchasePrice: Number,
+    quantity: Number,
+    unit: String
+});
+
+const Items = mongoose.model("Items", itemSchema)//table name/collection Name
+module.exports=Items;
+//API 1 -Create Item API
+app.post("/api/create-item", async (req, res) => {
+    try {
+        const { name, description, sellingPrice, purchasePrice, quantity, unit } = req.body;
+
+        const saveItem = new Items({
+            name,
+            description,
+            sellingPrice,
+            purchasePrice,
+            quantity,
+            unit
+        });
+
+        await saveItem.save();
+
+        res.status(201).json({message:"Item Created.",data:saveItem});
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: "Error creating item" });
+    }
+});
+
+//API 1 -Update item
+// app.put("/api/update-item", (req, res) => {
+//      try {
+//         const { name, description, sellingPrice, purchasePrice, quantity, unit } = req.body;
+
+//         const saveItem = new Items({
+//             name,
+//             description,
+//             sellingPrice,
+//             purchasePrice,
+//             quantity,
+//             unit
+//         });
+
+//         await saveItem.save();
+
+//         res.status(201).json(saveItem);
+
+//     } catch (err) {
+//         console.log(err);
+//         res.status(500).json({ message: "Error updating item" });
+//     }
+// })
+
+//API 1 -Delete item
+app.delete("/api/delete-item", (req, res) => {
+    try {
+
+    }
+    catch (err) {
+        console.log(err)
+    }
+})
+
+//API 1 -GetAll item
+app.get("/api/GetAll-item", async(req, res) => {
+    try {
+    
+        const items=await Items.find()
+        res.status(200).json({message:"List of items..",data:items})
+    }
+    catch (err) {
+        console.log(err)
+    }
+})
+
+
+// health Route
+app.get("health/", (req, res) => {
+    res.send("Server Running..");
+});
+
+const PORT = 9090
+app.listen(PORT, () => {
+    console.log('Server Started on port.', PORT)
+})
--- 3Month_Mar/CRUD Project/frontend/vite-project/src/App.jsx
+++ 3Month_Mar/CRUD Project/frontend/vite-project/src/App.jsx
@@ -1,22 +1,160 @@
+import { ToastContainer, toast } from 'react-toastify';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Style.css'
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Table from 'react-bootstrap/Table';
+import { useState } from 'react';
 
 function App() {
- 
+  const [itemName, setItemName] = useState()
+  
+  const handleOnchange = (event) => 
+    {
+    setItemName(event.target.value)
+    console.log('Typing in item name input field value==>',itemName);
+  }
+
+
+  function submitForm(e) {
+    e.preventDefault();
+    console.log('Form Submitted');
+    toast.success('🦄Form Submitted!', {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: false,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
+  }
 
   return (
     <>
-    <h2 className='text-danger'>Welcome Mahendra Mernstack Started..</h2>
-    <div className='container'>
-      <div className='row'>
-        <div className='col-md-6'>
-  <h3 className='border text-center'>  Add item</h3>
-        </div>
-        <div className='col-md-6'>
-           <h3 className='border text-center'> item list </h3>
+      <h2 className='text-danger'>Welcome Mahendra Mernstack Started..</h2>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-md-6'>
+            <h3 className='border text-center'>  Create item</h3>
+            <Form className='my-3'>
+              <Row className="mb-3">
+                <Form.Group as={Col} controlId="formGridEmail">
+                  <Form.Label>item Name</Form.Label>
+                  <Form.Control type="text" placeholder="Enter item Name" onChange={() => handleOnchange(event)} />
+                </Form.Group>
+                <Form.Group as={Col} controlId="formGridZip">
+                  <Form.Label>discription</Form.Label>
+                  <Form.Control type='text' placeholder='Enter Description' />
+                </Form.Group>
+              </Row>
+              <Row className='mb-3'>
+                <Form.Group as={Col} controlId="formGridPassword">
+                  <Form.Label>purchase price</Form.Label>
+                  <Form.Control type="number" placeholder="Password" />
+                </Form.Group>
+
+
+                <Form.Group as={Col} controlId="formGridAddress1">
+                  <Form.Label>selling price</Form.Label>
+                  <Form.Control type='number' placeholder="Enter selling price" />
+                </Form.Group>
+              </Row>
+              <Row className="mb-3">
+                <Form.Group as={Col} controlId="formGridCity">
+                  <Form.Label>quantity</Form.Label>
+                  <Form.Control type='number' placeholder='enter quantity' />
+                </Form.Group>
+
+                <Form.Group as={Col} controlId="formGridState">
+                  <Form.Label>Unit</Form.Label>
+                  <Form.Select defaultValue="Choose Unit...">
+                    <option>Choose...</option>
+                    <option>price</option>
+                    <option>Box</option>
+                    <option>Kg</option>
+                    <option>Gram</option>
+                    <option>Litter</option>
+                  </Form.Select>
+                </Form.Group>
+
+
+              </Row>
+              <div className='text-center' >
+                <Button variant="primary" type="submit" onClick={submitForm} className='w-50' >
+                  Submit
+                </Button>
+                <ToastContainer position="top-right"
+                  autoClose={5000}
+                  hideProgressBar={false}
+                  newestOnTop={false}
+                  closeOnClick={false}
+                  rtl={false}
+                  pauseOnFocusLoss
+                  draggable
+                  pauseOnHover
+                  theme="light" />
+              </div>
+            </Form>
+          </div>
+          <div className='col-md-6'>
+            <h3 className='border text-center'> Get items </h3>
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>Id</th>
+                  <th>item Name</th>
+                  <th>Description</th>
+                  <th>Purchase price</th>
+                  <th>Selling price</th>
+                  <th>Quantity</th>
+                  <th>Unit</th>
+                  <th>Action</th>
+
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>Pen</td>
+                  <td>Renolds pen</td>
+                  <td>10</td>
+                  <td>20</td>
+                  <td>45</td>
+                  <td>Box</td>
+                  <td className='d-flex'>
+                    <button className='btn btn-success'>Edit</button>
+                    <button className='btn btn-danger mx-2'>Delete</button>
+
+                  </td>
+
+                </tr>
+                <tr>
+                  <td>2</td>
+                  <td>Book</td>
+                  <td> noteb</td>
+                  <td>10</td>
+                  <td>20</td>
+                  <td>45</td>
+                  <td>Box</td>
+                  <td className='d-flex'>
+                    <button className='btn btn-success'>Edit</button>
+                    <button className='btn btn-danger mx-2'>Delete</button>
+
+                  </td>
+
+                </tr>
+
+
+
+              </tbody>
+            </Table>
+          </div>
         </div>
       </div>
-    </div>
     </>
   )
 }
